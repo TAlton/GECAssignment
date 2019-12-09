2e047d0c3f54757ccdbcd5046d16f356846e4d8a
tom <alton.thomas.97@hotmail.co.uk>
2019-12-09 15:22:48 +0000

    Implementing Graphics

---
+++ HAPI_Start/HAPI_Start/Graphics.cpp
@@ -0,0 +1,20 @@
+#include "Graphics.h"
+
+std::shared_ptr<Graphics> Graphics::s_pGraphics = nullptr;
+
+Graphics::Graphics() {
+
+
+
+}
+
+void Graphics::CreateWindow() {
+
+
+
+}
+
+
+Graphics::~Graphics()
+{
+}
---
+++ HAPI_Start/HAPI_Start/Graphics.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <HAPI_lib.h>
+
+using namespace HAPISPACE;
+
+class Graphics {
+
+public:
+
+	~Graphics();
+
+	static std::shared_ptr<Graphics>& GetInstance() {
+
+		if (!s_pGraphics) {
+
+			s_pGraphics.reset(new Graphics());
+
+		}
+
+		return s_pGraphics;
+
+	}
+
+private:
+
+	Graphics();
+
+	static std::shared_ptr<Graphics> s_pGraphics;
+
+	unsigned char* m_pScreen{ nullptr };
+
+	const int m_cnWidth{ 1280 },
+		m_cnHeight{ 720 },
+		m_cnScreenSize{ (m_cnWidth * m_cnHeight) << 2 };
+
+	const std::string m_strTitle{ "" };
+
+	void CreateWindow();
+
+};
+
