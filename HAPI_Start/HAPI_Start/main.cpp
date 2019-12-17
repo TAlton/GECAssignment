3c3c9033adfe6218f8490c65ad4bc0bdf8b65230
tom <alton.thomas.97@hotmail.co.uk>
2019-12-17 21:44:13 +0000

    update of todo

--- HAPI_Start/HAPI_Start/main.cpp
+++ HAPI_Start/HAPI_Start/main.cpp
@@ -12,9 +12,6 @@ void HAPI_Main() {
 enemy
 
 win lose conditions ---- kill boss or die
-implement more error handling
-----try catch blocks, file and texture loading
-check const correctness
 sound
 -----sound class, vector of sounds, enum for iteration i.e. bullet collision, playercollision
 health bar
